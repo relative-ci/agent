1de8672dea4b48e513aa24e712ddac35053f7ecb
Vio <vio@beanon.com>
2020-03-28 15:20:04 +0100

    fix: Mock webpack-plugin fetch

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,15 +1,17 @@
 const { exec } = require('child_process');
 
 describe('CLI', () => {
-  test('should return error if config is missing', () => {
+  test('should return error if config is missing', (done) => {
     exec('./bin/index.js', (error, stdout, sterr) => {
       expect(sterr).toContain('relativeci.config.js file is missing!');
+      done();
     });
   });
 
-  test('should return error if webpack stats is missing', () => {
+  test('should return error if webpack stats is missing', (done) => {
     exec('cd test/cli/missing-stats && ../../../bin/index.js', (error, stdout, sterr) => {
       expect(sterr).toContain('file does not exists');
+      done();
     });
   });
 });
--- test/webpack-plugin.test.js
+++ test/webpack-plugin.test.js
@@ -1,18 +1,44 @@
+jest.mock('isomorphic-fetch');
+
 const webpack = require('webpack');
 const MemoryFS = require('memory-fs');
+const fetch = require('isomorphic-fetch');
 
 const appConfig = require('./webpack/webpack.config');
 
+
 describe('webpack-plugin', () => {
-  test('should return error if RELATIVE_CI_KEY is missing', (done) => {
+  test('should send data to the service', (done) => {
+    process.env.CI = 'true';
+    process.env.RELATIVE_CI_ENDPOINT = 'http://localhost/save';
     process.env.RELATIVE_CI_KEY = '123';
     process.env.RELATIVE_CI_SLUG = 'organization/project';
+
+    fetch.mockReturnValue(Promise.resolve({
+      json: () => Promise.resolve({
+        res: {
+          job: {
+            internalBuildNumber: 1,
+          },
+        },
+        info: {
+          message: {
+            txt: 'Hello world!',
+          },
+        },
+      }),
+    }));
 
     const compiler = webpack(appConfig);
     compiler.outputFileSystem = new MemoryFS();
 
     compiler.run((error, stats) => {
-      expect(stats).toBeTruthy();
+      expect(error).toEqual(null);
+      expect(stats.hasErrors()).toBe(false);
+      expect(fetch).toHaveBeenCalledTimes(1);
+
+      process.env.CI = undefined;
+      process.env.RELATIVE_CI_ENDPOINT = undefined;
       process.env.RELATIVE_CI_KEY = undefined;
       process.env.RELATIVE_CI_SLUG = undefined;
       done();
