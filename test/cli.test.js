2d9dfbf93aa406bd9013686730fb6f3a5865fd21
Vio <vio@beanon.com>
2022-08-25 23:41:44 +0200

    test: CLI - add success case

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,4 +1,20 @@
+const http = require('http');
 const { exec } = require('child_process');
+
+const MOCK_RESULT = {
+  res: {
+    job: {
+      internalBuildNumber: 1,
+    },
+  },
+  info: {
+    message: {
+      txt: 'Hello world!',
+    },
+  },
+};
+
+const MOCK_SERVER_PORT = 5998;
 
 describe('CLI', () => {
   test('should return error if config is missing', (done) => {
@@ -20,5 +36,26 @@ describe('CLI', () => {
       expect(sterr).toContain('Invalid webpack stats structure');
       done();
     });
+  });
+
+  test('should run agent successfully', (done) => {
+    const server = http.createServer((req, res) => {
+      res.write(JSON.stringify(MOCK_RESULT));
+      res.end();
+    }).listen(MOCK_SERVER_PORT);
+
+    exec(
+      `cd test/cli/valid-data &&
+        RELATIVE_CI_ENDPOINT=http://localhost:${MOCK_SERVER_PORT}/save \
+        RELATIVE_CI_SLUG=org/project \
+        RELATIVE_CI_KEY=abc123 \
+        ../../../bin/index.js
+      `,
+      (_, stdout, sterr) => {
+        expect(stdout).toContain('Job #1 done.');
+        server.close();
+        done();
+      },
+    );
   });
 });
