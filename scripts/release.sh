35572f1d23885371065472ccc5418ee21de6b5f5
Vio <vio@beanon.com>
2023-07-27 23:21:50 +0200

    build: Add NPM_CONFIG_PROVENANCE inline

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,4 +3,4 @@
 TAG=$(git describe --tags --abbrev=0)
 VERSION=${TAG:1}
 
-npx release-it $VERSION --config ./config/release-it-publish.js --ci
+NPM_CONFIG_PROVENANCE=true npx release-it $VERSION --config ./config/release-it-publish.js --ci
