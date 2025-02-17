8f2616f28868e4ddf9988533612799a8f217caee
Viorel Cojocaru <vio@beanon.com>
2025-02-18 00:12:44 +0100

    test: Ingest - add tests

--- src/__tests__/ingest.ts
+++ src/__tests__/ingest.ts
@@ -10,7 +10,7 @@ describe('Ingest', () => {
     jest.clearAllMocks();
   });
 
-  test('send data to the ingest endpoint', async () => {
+  test('should ingest data', async () => {
     fetch.mockResolvedValueOnce({
       json: () => Promise.resolve({
         res: {
@@ -30,13 +30,53 @@ describe('Ingest', () => {
       key: 'abc-123',
       endpoint: 'http://localhost',
       agentVersion: '0.0.0',
-      slug: 'org/project',
+      slug: 'organization/project',
+      commit: 'abcd1234',
+      branch: 'master',
+      build: '123',
+      buildUrl: '#',
+      pr: '12',
+      commitMessage: 'Commit message',
+    });
+
+    expect(fetch).toHaveBeenCalledWith(
+      'http://localhost',
+      {
+        body: JSON.stringify({
+          key: 'abc-123',
+          project: 'organization/project',
+          agentVersion: '0.0.0',
+          job: {
+            commit: 'abcd1234',
+            branch: 'master',
+            prNumber: '12',
+            buildNumber: '123',
+            buildUrl: '#',
+            commitMessage: 'Commit message',
+          },
+          rawData: {},
+        }),
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+        method: 'POST',
+      },
+    );
+  });
+
+  test.only('should show error when the ingest action fails', async () => {
+    fetch.mockRejectedValueOnce(new Error('Network error'));
+
+    await ingest({}, {
+      key: 'abc-123',
+      endpoint: 'http://localhost',
+      agentVersion: '0.0.0',
+      slug: 'organization/project',
       commit: 'abcd1234',
       branch: 'master',
     });
 
     expect(fetch).toHaveBeenCalled();
+    expect(fetch).rejects.toBeTruthy();
   });
-
-  test('received invalid data from the ingest endpoint', () => {});
 });
