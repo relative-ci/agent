7d044499b9139eba28c33acac79a7622af18829a
Viorel Cojocaru <vio@beanon.com>
2025-03-14 22:05:09 +0100

    build: Replace jest with vitest

--- test/webpack/webpack4/webpack-plugin.test.ts
+++ test/webpack/webpack4/webpack-plugin.test.ts
@@ -1,27 +1,32 @@
-const webpack = require('webpack');
-const MemoryFS = require('memory-fs');
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  afterEach, describe, expect, test, vi,
+} from 'vitest';
+import webpack from 'webpack';
+import MemoryFS from 'memory-fs';
 
-const webpackStats = require('../../__snapshots__/webpack-4-stats.json');
-const appConfig = require('./webpack.config');
-const appFailOnErrorConfig = require('./webpack-fail-on-error.config');
-const {
+// eslint-disable-next-line import/no-relative-packages
+import webpackStats from '../../__snapshots__/webpack-4-stats.json';
+import {
   ENV_DEFAULT, INGEST_MOCK, clearCustomEnv, getMockRequest, setCustomEnv,
-} = require('../../utils');
+} from '../../utils'; // eslint-disable-line import/no-relative-packages
+import appConfig from './webpack.config';
+import appFailOnErrorConfig from './webpack-fail-on-error.config';
 
 describe('webpack-plugin / webpack4', () => {
   afterEach(() => {
     clearCustomEnv();
-    jest.clearAllMocks();
+    vi.clearAllMocks();
   });
 
-  test('should ingest data successfully', (done) => {
+  test('should ingest data successfully', () => new Promise((done) => {
     setCustomEnv();
 
-    global.fetch = jest.fn(() => Promise.resolve({
+    global.fetch = vi.fn(() => Promise.resolve({
       json: () => Promise.resolve(INGEST_MOCK),
     }));
 
-    const compiler = webpack(appConfig);
+    const compiler = webpack({ ...appConfig, context: __dirname });
     compiler.outputFileSystem = new MemoryFS();
 
     compiler.run((error, stats) => {
@@ -42,14 +47,14 @@ describe('webpack-plugin / webpack4', () => {
         }),
       );
 
-      done();
+      done(true);
     });
-  });
+  }));
 
-  test('should warn, not ingest and not throw on params error', (done) => {
+  test('should warn, not ingest and not throw on params error', () => new Promise((done) => {
     setCustomEnv({ RELATIVE_CI_KEY: '' });
 
-    const compiler = webpack(appConfig);
+    const compiler = webpack({ ...appConfig, context: __dirname });
     compiler.outputFileSystem = new MemoryFS();
 
     compiler.run((error, stats) => {
@@ -57,32 +62,33 @@ describe('webpack-plugin / webpack4', () => {
       expect(error).toEqual(null);
       expect(fetch).not.toHaveBeenCalled();
 
-      done();
+      done(true);
     });
-  });
+  }));
 
-  test('should warn and not throw on ingest error', (done) => {
+  test('should warn and not throw on ingest error', () => new Promise((done) => {
     setCustomEnv();
 
-    global.fetch = jest.fn(() => Promise.rejest(new Error('Network error')));
+    global.fetch = vi.fn(() => Promise.rejest(new Error('Network error')));
 
-    const compiler = webpack(appConfig);
+    const compiler = webpack({ ...appConfig, context: __dirname });
     compiler.outputFileSystem = new MemoryFS();
 
     compiler.run((error, stats) => {
       expect(error).toEqual(null);
       expect(stats.hasErrors()).toBe(false);
       expect(fetch).toHaveBeenCalledTimes(1);
-      done();
-    });
-  });
 
-  test('should throw and fail on ingest error when failOnError is true', (done) => {
+      done(true);
+    });
+  }));
+
+  test('should throw and fail on ingest error when failOnError is true', () => new Promise((done) => {
     setCustomEnv();
 
-    global.fetch = jest.fn(() => Promise.rejest(new Error('Network error')));
+    global.fetch = vi.fn(() => Promise.rejest(new Error('Network error')));
 
-    const compiler = webpack(appFailOnErrorConfig);
+    const compiler = webpack({ ...appFailOnErrorConfig, context: __dirname });
     compiler.outputFileSystem = new MemoryFS();
 
     try {
@@ -90,11 +96,11 @@ describe('webpack-plugin / webpack4', () => {
         expect(error).toBeNull();
         expect(stats.hasErrors()).toBe(true);
         expect(stats.toJson().errors[0]).toMatch(/Error ingesting data/);
-        done();
+        done(true);
       });
     } catch (err) {
       console.log(err);
-      done();
+      done(true);
     }
-  });
+  }));
 });
