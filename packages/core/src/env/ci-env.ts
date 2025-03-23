6683c13e6efc8a2e2ec0bb919bedfd04d97f2ce7
Viorel Cojocaru <vio@beanon.com>
2025-03-23 10:15:34 +0100

    feat(core): Env - load extra data from github event data

--- packages/core/src/env/ci-env.ts
+++ packages/core/src/env/ci-env.ts
@@ -1,37 +1,57 @@
 // env-ci and its dependencies are not flagged as external (not bundled)
 // eslint-disable-next-line import/no-extraneous-dependencies
-import envCi, { JenkinsEnv, type CiEnv as BaseCiEnv } from 'env-ci';
+import envCi, { type CiEnv as BaseCiEnv } from 'env-ci';
 
 import { getSlugFromGitURL } from './git/slug';
+import { getGitHubEnv } from './service/github';
 
 export type CiEnv = {
   isCi: boolean;
   slug?: string;
   service?: string;
   branch?: string;
-  prBranch?: string;
   commit?: string;
+  commitMessage?: string;
   pr?: string;
   build?: string;
   buildUrl?: string;
 };
-
-type JenkinsEnvWithSlug = JenkinsEnv & { slug: string };
 
 /**
  * Load environment variables - fallback to env-ci environment variables
  */
 export function getCiEnv(): CiEnv {
   // CI environment variables
-  const ciEnvVars: BaseCiEnv | JenkinsEnvWithSlug = envCi();
+  const ciEnv: BaseCiEnv = envCi();
+
+  let env = {
+    isCi: ciEnv.isCi,
+    slug: 'slug' in ciEnv ? ciEnv.slug : undefined,
+    service: 'service' in ciEnv ? ciEnv.service : undefined,
+    /**
+     * When running during pull_request, env-ci exposes the current branch as prBranch
+     */
+    // eslint-disable-next-line no-nested-ternary
+    branch: 'prBranch' in ciEnv ? ciEnv.prBranch : ('branch' in ciEnv ? ciEnv.branch : undefined),
+    commit: 'commit' in ciEnv ? ciEnv.commit : undefined,
+    pr: 'pr' in ciEnv ? ciEnv.pr : undefined,
+    build: 'build' in ciEnv ? ciEnv.build : undefined,
+    buildUrl: 'buildUrl' in ciEnv ? ciEnv.buildUrl : undefined,
+  };
 
   // env-ci does not provide a slug for jenkins
   // https://github.com/semantic-release/env-ci/blob/master/services/jenkins.js#LL18
   // https://www.jenkins.io/doc/book/pipeline/jenkinsfile/#using-environment-variables
   // https://plugins.jenkins.io/git/#plugin-content-environment-variables
-  if ('service' in ciEnvVars && ciEnvVars.service === 'jenkins') {
-    (ciEnvVars as JenkinsEnvWithSlug).slug = getSlugFromGitURL(process.env.GIT_URL);
+  if (env.service === 'jenkins' && !env.slug) {
+    env.slug = getSlugFromGitURL(process.env.GIT_URL);
   }
 
-  return ciEnvVars;
+  // Service data
+  if (process.env.GITHUB_EVENT_PATH) {
+    const gitHubEnv = getGitHubEnv(process.env.GITHUB_EVENT_PATH);
+    env = { ...env, ...gitHubEnv };
+  }
+
+  return ciEnv;
 }
---
+++ packages/core/src/env/service/github.ts
@@ -0,0 +1,43 @@
+import fs from 'fs-extra';
+import type { PushEvent, PullRequestEvent, WorkflowRunEvent } from '@octokit/webhooks-types';
+
+type GitHubEventData = PushEvent | PullRequestEvent | WorkflowRunEvent;
+
+export type GitHubEnv = {
+  branch?: string;
+  commit?: string;
+  commitMessage?: string;
+};
+
+export function getGitHubEnv(eventFilepath: string): GitHubEnv {
+  const env: GitHubEnv = {};
+
+  let payload: GitHubEventData;
+
+  try {
+    payload = fs.readJSONSync(eventFilepath) as GitHubEventData;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.warn(`Error reading GitHub data from ${eventFilepath}`, error.message);
+  }
+
+  // push
+  if ('head_commit' in payload) {
+    env.commit = payload.head_commit.id;
+    env.commitMessage = payload.head_commit.message;
+  }
+
+  // pull request
+  if ('pull_request' in payload) {
+    env.commit = payload.pull_request.head?.sha;
+    env.branch = payload.pull_request.head?.ref;
+  }
+
+  // workflow_run
+  if ('workflow_run' in payload) {
+    env.commit = payload.workflow_run.head_commit?.id;
+    env.commitMessage = payload.workflow_run.head_commit?.message;
+  }
+
+  return env;
+}
