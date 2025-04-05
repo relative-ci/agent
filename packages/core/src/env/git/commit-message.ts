7b017ef2aff6e800af633e97c65b6ca8178adbee
Viorel Cojocaru <vio@beanon.com>
2025-04-05 12:42:25 +0200

    feat: Env - use fallbacks to get commit message for pull_request events

--- packages/core/src/env/git/commit-message.ts
+++ packages/core/src/env/git/commit-message.ts
@@ -1,10 +1,10 @@
 import childProcess from 'child_process';
 
-export function getGitCommitMessage(): string {
+export function getGitCommitMessage(commit: string = ''): string {
   let message = '';
 
   try {
-    message = childProcess.execSync('git log -1 --pretty=%B').toString().trim();
+    message = childProcess.execSync(`git show -s --format=%B ${commit}`).toString().trim();
   } catch (error) {
     console.error('Error reading commit message from git', error);
   }
