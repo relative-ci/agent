11227628909cffd1709cf8b8a41604d9f99d1edc
Viorel Cojocaru <vio@beanon.com>
2026-05-29 20:42:03 +0200

    build: Verdaccio - add cleanup

--- scripts/local-registry.sh
+++ scripts/local-registry.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-echo "Setup npmrc"
-npm config set registry=http://localhost:4873
-npm config set //localhost:4873/:_authToken=fooBar # Add a non empty token for npm 6
+LOCAL_REGISTRY="http://localhost:4873"
+LOCAL_REGISTRY_AUTH="//localhost:4873/:_authToken=foobar" # Add a non empty auth token for npm +6
+
+cleanup() {
+  echo "Reverting .npmrc changes"
+  mv .npmrc.bak .npmrc
+}
+
+trap cleanup EXIT
+
+echo "Setup .npmrc"
+cp .npmrc .npmrc.bak
+npm config set registry=$LOCAL_REGISTRY --location project
+npm config set $LOCAL_REGISTRY_AUTH --location project
 
 echo "Run local registry"
 npx verdaccio --config ./verdaccio.yaml
-
-echo "Wait for the start"
-sleep 10
